92a50190aa8cbf00ec4f5adf8345ac18a8e3cef8
mattiaitaliano <mattia94italiano@gmail.com>
2023-01-21 09:58:20 +0000

    Create main file

---
+++ main.py
@@ -0,0 +1,8 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import widget
+from kivy.uix.label import Label
+
+class MyLab(App):
+    def __init__(self):
+        return Label(text="My Lab") 
